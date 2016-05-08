e12cd62648e1ee834aada025efe430b85f43adb7
Seth MacPherson <smacpherson@barkleyus.com>
2016-05-07 19:01:23 -0500

    #0.0.2 - Adjusting import from ES2015 to module.exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
+#0.0.2 - Adjusting import from ES2015 to module.exports
+
 #0.0.1 - Initial Concept
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import UA from 'lib/ua.js';
 const aguid = require('aguid')
 const user = require('git-user-email')
 
-export default function init(uid) {
+module.exports = function(uid) {
   const guid = aguid(user());
   const ga = new UA(uid, guid);
   return ga;
